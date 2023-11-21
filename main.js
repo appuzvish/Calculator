535fa99240b7c63a08b25a1e2bc1de9d05471b12
appuzvish <vishnulk73@gmail.com>
2023-11-21 18:11:24 +0530

    Calculator

---
+++ main.js
@@ -0,0 +1,18 @@
+const displayData = (data)=>{
+    result.value += data
+
+}
+const calculatorClear =()=>{
+    result.value=""
+}
+const calculateResult = ()=>{
+    if(result.value!=""){
+        try{result.value= eval(result.value)}
+        catch{
+            result.value = "Erorr!!!"
+        }
+    }
+}
+const removeLast = ()=>{
+    result.value = result.value.slice(0,-1)
+}
